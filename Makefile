467fc539bc2b2a03e440ec6d0b126551359259cd
newOnahtaN <ncowen@email.wm.edu>
2015-11-10 11:53:13 -0500

    created gauss method in c++

--- Makefile
+++ Makefile
@@ -1,8 +1,16 @@
 test: test.cpp
 	g++ test.cpp -o test -O2 -larmadillo
 
+all: fbeuler_jb fbeuler_gs fbtrap_jb
+
 fbeuler_jb: fbeuler_jb.cpp
 	g++ fbeuler_jb.cpp -o fbeuler_jb -O2 -larmadillo
+
+fbeuler_gs: fbeuler_gs.cpp
+	g++ fbeuler_gs.cpp -o fbeuler_gs -O2 -larmadillo
+
+fbtrap_jb: fbtrap_jb.cpp
+	g++ fbtrap_jb.cpp -o fbtrap_jb -O2 -larmadillo
 
 clean:
 	rm test fbeuler_jb
--- fbeuler_jb.cpp
+++ fbeuler_jb.cpp
@@ -7,7 +7,7 @@ using namespace arma;
 
 
 double dx(vec t, double x, double y){
-  return (-x+2)*y + pow((x-y),2);
+  return -x + (2*y) + pow((x-y),2);
 }
 
 double dy(vec t, double x, double y){
@@ -16,29 +16,30 @@ double dy(vec t, double x, double y){
 
 void fbeuler_jb( double x0, double T, long N, double tol, long maxiter){
   double h = (T+1)/N;
-  //vector t = 0:h:T use span
-  vec x = x0 * ones<vec>(5/*t.n_rows*/);
-  vec y = zeros<vec>(5/*t.n_rows*/);
-  vec xlast = x * 0;
-  vec ylast = y * 0;
-
+  vec t = linspace<vec>(0,h,T);
+  vec x = x0 * ones<vec>(t.n_rows);
+  vec y = zeros<vec>(t.n_rows);
+  vec xlast = zeros<vec>(t.n_rows);;
+  vec ylast = zeros<vec>(t.n_rows);;
   vec convInd = zeros<vec>(maxiter);
+  long m = 0;
 
-  long m = 1;
-  while ((m < maxiter) && max(norm(y-ylast), norm(x-xlast)) > tol){
+  do {
     xlast = x;
-    for (long i = 0; i < 5/*t.n_rows*/-1; i++ ){
-      x(i-1) = xlast(i) - h * dx(/*t*/,xlast(i),ylast(i));
+    for (long i = 0; i < t.n_rows-1; i++){
+      x(i+1) = xlast(i) + h * dx(t,xlast(i),ylast(i));
     }
 
     ylast = y;
-    for (long i = 5/*t.n_rows*/-1; i > 0; i-- ){
-      y(i-1) = ylast(i) - h * dy(/*t*/,xlast(i),ylast(i));
+    for (long i = t.n_rows-1; i > 0; i--){
+      y(i-1) = ylast(i) - h * dy(t,xlast(i),ylast(i));
     }
 
     convInd[m] = max(norm(y-ylast), norm(x-xlast));
+    printf("%6.13f\n", convInd[m]);
+
     m++;
-  }
+  } while ((m < maxiter) && convInd[m-1] > tol);
 }
 
 int main(int argc, char** argv)
